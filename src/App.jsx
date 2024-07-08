5cecdcd9bb33fcee6ce5972c75e7825e7126a37f
Aashik9567 <aashikmahato9567@gmail.com>
2024-07-08 23:51:31 +0545

    final code

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todoo from './components/Todoo'
+
+function App() {
+  return (
+    <>
+    <h1 className='mb-5'>Todo App</h1>
+     <AddTodo/>
+     <Todoo/>
+    </>
+  )
+}
+
+export default App
